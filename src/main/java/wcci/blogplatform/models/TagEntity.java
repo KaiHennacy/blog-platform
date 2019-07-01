b82750ab9c302d42a1fa2a2fe41d13e85c7410d7
KaiHennacy <kchennacy@gmail.com>
2019-07-01 09:54:46 -0400

    final(ish) version of mastery project

--- src/main/java/wcci/blogplatform/models/TagEntity.java
+++ src/main/java/wcci/blogplatform/models/TagEntity.java
@@ -47,8 +47,6 @@ public class TagEntity {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((posts == null) ? 0 : posts.hashCode());
 		return result;
 	}
 
@@ -66,17 +64,8 @@ public class TagEntity {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (posts == null) {
-			if (other.posts != null)
-				return false;
-		} else if (!posts.equals(other.posts))
-			return false;
 		return true;
 	}
-	
+
+		
 }
